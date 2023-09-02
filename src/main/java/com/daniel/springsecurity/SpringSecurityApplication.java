7db44e18969bf8f7e62ac6351fadb07bc76f3861
Daniel Penelva <d4n.andrade@gmail.com>
2023-09-02 02:07:53 -0300

    Criando a Classe HttpController e o método de uma rota publica

--- src/main/java/com/daniel/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/daniel/springsecurity/SpringSecurityApplication.java
@@ -2,6 +2,8 @@ package com.daniel.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -12,3 +14,13 @@ public class SpringSecurityApplication {
 
 }
 
+@RestController
+class HttpController{
+
+	// http://localhost:8080/public
+	@GetMapping("/public")
+	String publicRoute(){
+		return "<h1> Via pública, fique à vontade para dar uma olhada! </h1>";
+	}
+
+}
